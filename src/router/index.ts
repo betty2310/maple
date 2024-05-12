28ff3675dc8c9669c862c0b1c52c6876beeb3b0b
betty2310 <huynh.dh2310@gmail.com>
2024-05-13 00:45:39 +0700

    feat(route): initial route

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/signin',
+      name: 'signin',
+      component: () => import('@/views/SigninView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col h-screen">
+    <div>
+      <ToolBar />
+    </div>
+    <div class="flex flex-1">
+      <ActivityBar />
+      <div :class="layoutStore.isShowLeftPanel ? 'ml-[28rem]' : 'ml-14'" class="flex-auto mr-0 mb-6 mt-9"
+           @drop="onDrop">
+        <MainCircuit />
+      </div>
+    </div>
+    <div>
+      <StatusBar />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import ToolBar from '@/components/ToolBar.vue'
+import ActivityBar from '@/components/ActivityBar.vue'
+import StatusBar from '@/components/StatusBar.vue'
+import MainCircuit from '@/components/MainCircuit.vue'
+import { useLayoutStore } from '@/stores/layoutStore'
+import useDragAndDrop from '@/hooks/useDnDCircuitComponent'
+
+
+const { onDrop } = useDragAndDrop()
+
+const layoutStore = useLayoutStore()
+</script>
