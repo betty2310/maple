f06a6e18d2e19fd609c4704af5107d898126ed8f
huynhdh <duong.huu.huynh@sun-asterisk.com>
2024-05-13 16:27:41 +0700

    feat(components): add UI input and label components

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,8 +1,76 @@
-<template>
-  <Button>Click me</Button>
-</template>
-
-<script lang="ts" setup>
-// Import predefined theme
+<script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+import { useField, useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as zod from 'zod';
+const validationSchema = toTypedSchema(
+  zod.object({
+    email: zod.string().min(1, { message: 'This is required' }).email({ message: 'Must be a valid email' }),
+    password: zod.string().min(1, { message: 'This is required' }).min(8, { message: 'Too short' }),
+  })
+);
+const { handleSubmit, errors } = useForm({
+  validationSchema,
+});
+const { value: email } = useField('email');
+const { value: password } = useField('password');
+const onSubmit = handleSubmit(values => {
+  alert(JSON.stringify(values, null, 2));
+});
+
 </script>
+
+<template>
+  <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div class="flex items-center justify-center py-12">
+      <div class="mx-auto grid w-[350px] gap-6">
+        <div class="grid gap-2 text-center">
+          <h1 class="text-3xl font-bold">
+            Login
+          </h1>
+          <p class="text-balance text-muted-foreground">
+            Enter your email below to login to your account
+          </p>
+        </div>
+        <form @submit="onSubmit">
+          <div class="grid gap-4">
+            <div class="grid gap-2">
+              <Label for="email">Email</Label>
+              <Input id="email" v-model="email" type="email" placeholder="m@example.com" required />
+              <span>{{ errors.email }}</span>
+            </div>
+            <div class="grid gap-2">
+              <div class="flex items-center">
+                <Label for="password">Password</Label>
+                <a href="/forgot-password" class="ml-auto inline-block text-sm underline">
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" v-model="password" type="password" required />
+              <span>{{ errors.password }}</span>
+            </div>
+            <Button type="submit" class="w-full">
+              Login
+            </Button>
+            <Button variant="outline" class="w-full">
+              Login with Google
+            </Button>
+          </div>
+        </form>
+        <div class="mt-4 text-center text-sm">
+          Don't have an account?
+          <a href="#" class="underline">
+            Sign up
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="hidden bg-muted lg:block">
+      <img src="../assets/preview.jpg" alt="Image" width="1920" height="1080"
+        class="h-full w-full object-fill dark:brightness-[0.2] dark:grayscale">
+    </div>
+  </div>
+</template>
